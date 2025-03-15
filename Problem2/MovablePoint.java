2c9f15260b4bd0518ba80e785faab8e175cfe4c6
JaowadH <jaowad.hossain@keyin.com>
2025-03-15 17:44:46 -0230

    Added Point and Moveable Point

---
+++ Problem2/MovablePoint.java
@@ -0,0 +1,19 @@
+public class MovablePoint extends Point {
+    private float xSpeed, ySpeed;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public void move() {
+        setX(getX() + xSpeed);
+        setY(getY() + ySpeed);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Speed=(" + xSpeed + ", " + ySpeed + ")";
+    }
+}
